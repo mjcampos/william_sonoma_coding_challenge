8ab7ec95671a12e147f29635f17cf050c5cb3ac4
Michael Campos <camposmj89@gmail.com>
2019-07-12 14:24:42 -0700

    Initial setup that involves removing useless components and setting up a basic App component with centered title

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Container} from 'react-bootstrap';
+
+class App extends Component {
+	render() {
+		return (
+			<Container>
+				<h1 className="text-center">Williams-Sonoma Coding Challenge</h1>
+			</Container>
+		);
+	}
+}
+
+export default App;
