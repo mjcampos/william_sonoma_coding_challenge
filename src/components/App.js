2f63bf4f7d6f3e22b22e297252bf0dbf170f1d0b
Michael Campos <camposmj89@gmail.com>
2019-07-17 16:09:43 -0700

    Created an Item component and built it out

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,40 @@
 import React, {Component} from 'react';
-import {Container} from 'react-bootstrap';
+import {connect} from 'react-redux';
+import {Container, Row, Col} from 'react-bootstrap';
+import Item from './Item';
 
 class App extends Component {
 	render() {
+		var {items} = this.props;
+		var rows = [];
+		var numOfCols = 3;
+
+		for(var i = 0; i < items.length; i += numOfCols) {
+			rows.push(items.slice(i, i + numOfCols));
+		}
+
 		return (
 			<Container>
 				<h1 className="text-center">Williams-Sonoma Coding Challenge</h1>
+
+				{rows.map((row, i) => (
+					<Row key={i}>
+						{row.map(item => (
+							<Col key={item.id}>
+								<Item {...item}/>
+							</Col>
+						))}
+					</Row>
+				))}
 			</Container>
 		);
 	}
 }
 
-export default App;
+var mapStateToProps = state => {
+	return {
+		items: state
+	}
+};
+
+export default connect(mapStateToProps, null)(App);
---
+++ src/components/Item.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+import {Card, Carousel} from 'react-bootstrap';
+
+class Item extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			displayCarousel: false
+		}
+	}
+
+	render() {
+		return (
+			<Card>
+				<div onClick={() => this.setState({displayCarousel: true})}>
+					{
+						this.state.displayCarousel ? (
+							<Carousel>
+								{this.props.images.map((image, i) => (
+									<Carousel.Item key={i}>
+										<Card.Img  variant="top" src={image.href}/>
+									</Carousel.Item>
+								))}
+							</Carousel>
+						) : (
+							<Card.Img variant="top" src={this.props.thumbnail.href} />
+						)
+					}
+				</div>
+
+				<Card.Body>
+					<Card.Title>{this.props.name}</Card.Title>
+
+						<p>Prices:</p>
+
+						<ul>
+							<li>Regular:
+								<ul>
+									<li>High: ${this.props.priceRange.regular.high}</li>
+									<li>Low: ${this.props.priceRange.regular.low}</li>
+								</ul>
+							</li>
+
+							<li>Selling:
+								<ul>
+									<li>High: ${this.props.priceRange.selling.high}</li>
+									<li>Low: ${this.props.priceRange.selling.low}</li>
+								</ul>
+							</li>
+						</ul>
+				</Card.Body>
+			</Card>
+		);
+	}
+}
+
+export default Item;
